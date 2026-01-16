e544123dc599206d2bc13f2cdb078f263f5bcb0a
lil David <1337lilDavid@gmail.com>
2026-01-15 21:07:37 -0600

    Create base-level AP files

---
+++ src/msr/__init__.py
@@ -0,0 +1,9 @@
+from worlds.AutoWorld import World
+
+from .data.constants import GAME_NAME
+
+
+class SamusReturnsWorld(World):
+    """TODO"""
+
+    game = GAME_NAME
---
+++ src/msr/data/constants.py
@@ -0,0 +1,1 @@
+GAME_NAME = "Metroid: Samus Returns"

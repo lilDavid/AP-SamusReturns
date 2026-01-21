c75e4ed2499cc4b98e9a3b788d06c052207c0882
lil David <1337lilDavid@gmail.com>
2026-01-21 05:59:43 -0600

    Factor area collection into new submodule

--- src/msr/patch.py
+++ src/msr/patch.py
@@ -15,6 +15,7 @@ from .data.constants import GAME_NAME
 from .data.internal_names import AreaId, ItemId, ItemModel, PickupSound
 from .items import ItemName, LauncherData, OtherItemData, TankData, UniqueItemData, item_data_table
 from .locations import location_table
+from .regions import all_areas_data
 
 if TYPE_CHECKING:
     from . import SamusReturnsWorld
@@ -204,36 +205,8 @@ class SamusReturnsPatch(APAutoPatchInterface):
         if hasattr(SamusReturnsPatch, "room_names"):
             return SamusReturnsPatch.room_names
 
-        from .data.region_data.area_1 import area_1_data
-        from .data.region_data.area_2 import area_2_entryway_data, area_2_exterior_data, area_2_interior_data
-        from .data.region_data.area_3 import area_3_caverns_data, area_3_exterior_data, area_3_interior_data
-        from .data.region_data.area_4 import area_4_caves_data, area_4_mines_data
-        from .data.region_data.area_5 import area_5_exterior_data, area_5_interior_data, area_5_lobby_data
-        from .data.region_data.area_6 import area_6_data
-        from .data.region_data.area_7 import area_7_data
-        from .data.region_data.area_8 import area_8_data
-        from .data.region_data.surface import surface_east_data, surface_west_data
-
         mapping: dict[AreaId, dict[str, str]] = {}
-        for area_data in (
-            surface_east_data,
-            area_1_data,
-            area_2_exterior_data,
-            area_2_interior_data,
-            area_2_entryway_data,
-            area_3_exterior_data,
-            area_3_caverns_data,
-            area_3_interior_data,
-            area_4_caves_data,
-            area_4_mines_data,
-            area_5_lobby_data,
-            area_5_exterior_data,
-            area_5_interior_data,
-            area_6_data,
-            area_7_data,
-            area_8_data,
-            surface_west_data,
-        ):
+        for area_data in all_areas_data:
             mapping[area_data.id] = {room.id: name for name, room in area_data.rooms.items()}
         SamusReturnsPatch.room_names = mapping
         return mapping
---
+++ src/msr/regions.py
@@ -0,0 +1,29 @@
+from .data.region_data.area_1 import area_1_data
+from .data.region_data.area_2 import area_2_entryway_data, area_2_exterior_data, area_2_interior_data
+from .data.region_data.area_3 import area_3_caverns_data, area_3_exterior_data, area_3_interior_data
+from .data.region_data.area_4 import area_4_caves_data, area_4_mines_data
+from .data.region_data.area_5 import area_5_exterior_data, area_5_interior_data, area_5_lobby_data
+from .data.region_data.area_6 import area_6_data
+from .data.region_data.area_7 import area_7_data
+from .data.region_data.area_8 import area_8_data
+from .data.region_data.surface import surface_east_data, surface_west_data
+
+all_areas_data = (
+    surface_east_data,
+    area_1_data,
+    area_2_entryway_data,
+    area_2_exterior_data,
+    area_2_interior_data,
+    area_3_caverns_data,
+    area_3_exterior_data,
+    area_3_interior_data,
+    area_4_caves_data,
+    area_4_mines_data,
+    area_5_exterior_data,
+    area_5_interior_data,
+    area_5_lobby_data,
+    area_6_data,
+    area_7_data,
+    area_8_data,
+    surface_west_data,
+)

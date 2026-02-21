0401103adf1c71f047f303664a3148225a75d9d4
lil David <1337lilDavid@gmail.com>
2026-02-21 00:22:33 -0600

    Import libraries into AP cache instead

--- src/msr/lib.py
+++ src/msr/lib.py
@@ -1,4 +1,5 @@
 import importlib.util
+import shutil
 import sys
 import zipfile
 from collections.abc import Sequence
@@ -7,27 +8,49 @@ from importlib.machinery import ModuleSpec
 from pathlib import Path
 from types import ModuleType
 
+import Utils
+
 
 class SamusReturnsLibFinder(MetaPathFinder):
-    """Lazily copies the contents of the lib path in this apworld into a temp dir"""
+    """Lazily copies the contents of the lib path in this apworld into the AP cache dir"""
 
+    checked_cache: bool
     module_cache: set[str] | None
     loaded: bool
 
     def __init__(self):
+        self.checked_cache = False
         self.module_cache = None
         self.loaded = False
 
     def find_spec(
         self, fullname: str, path: Sequence[str] | None, target: ModuleType | None = None
     ) -> ModuleSpec | None:
+        from . import SamusReturnsWorld
+
         if self.loaded:
             return None
 
         world = Path(__file__).parent
         apworld = zipfile.ZipFile(world.parent)
+
+        # If cached, use that if the version matches
+        cached_lib = Path(Utils.cache_path(f"{world.name}/lib"))
+        if not self.checked_cache:
+            self.checked_cache = True
+            try:
+                with open(cached_lib / "VERSION.txt") as version:
+                    cache_version = Utils.tuplize_version(version.read())
+                if cache_version == SamusReturnsWorld.world_version:
+                    self.loaded = True
+                    sys.path.append(str(cached_lib))
+                    return importlib.util.find_spec(fullname)
+            except FileNotFoundError:
+                pass
+
         lib = zipfile.Path(apworld, f"{world.name}/lib/")
 
+        # Try to wait until importer actually needs something from us (read: OSSR)
         if self.module_cache is None:
             self.module_cache = {
                 path.stem if path.suffix == ".py" else path.name
@@ -37,12 +60,23 @@ class SamusReturnsLibFinder(MetaPathFinder):
         if fullname not in self.module_cache:
             return None
 
-        import tempfile
-
+        # Extract the dependencies
         self.loaded = True
-        temp_dir = Path(tempfile.mkdtemp())
-        apworld.extractall(temp_dir, [name for name in apworld.namelist() if name.startswith(f"{world.name}/lib/")])
-        sys.path.append(str(temp_dir / f"{world.name}/lib"))
+
+        try:
+            shutil.rmtree(cached_lib)
+        except FileNotFoundError:
+            pass
+        cached_lib.mkdir(parents=True, exist_ok=True)
+
+        apworld.extractall(
+            Utils.cache_path(),
+            [name for name in apworld.namelist() if name.startswith(f"{world.name}/lib/")],
+        )
+        with open(cached_lib / "VERSION.txt", "w") as version:
+            version.write(SamusReturnsWorld.world_version.as_simple_string())
+
+        sys.path.append(str(cached_lib))
         return importlib.util.find_spec(fullname)
 
 

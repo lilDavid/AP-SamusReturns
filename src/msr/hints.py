98bcbe3e3a1e75083bf1d0f81865e787574c134c
lil David <1337lilDavid@gmail.com>
2026-05-16 09:03:00 -0500

    Add "always" location hints

--- src/msr/hints.py
+++ src/msr/hints.py
@@ -3,11 +3,12 @@ from __future__ import annotations
 import itertools
 import math
 from collections.abc import Iterable, Sequence
-from typing import TYPE_CHECKING, NamedTuple, cast
+from typing import TYPE_CHECKING, NamedTuple, TypeVar, cast
 
 from BaseClasses import Item, Location, LocationProgressType
 
 from .data.internal_names import AreaId
+from .data.room_names import Area6, Area8
 from .items import ItemName, major_items
 
 if TYPE_CHECKING:
@@ -54,66 +55,71 @@ hint_data = [
     HintData(AreaId.SURFACE_WEST, "LE_RandoDNA"),
 ]
 
+T = TypeVar("T")
 
 DNA_HINTS_PER_STATUE = 5
 
+# Always hint these locations
+# May change based on settings in future
+ALWAYS_HINTS = [
+    Area6.Diggernaut.location(),
+    Area6.ElectricEscalade.location(),
+    Area8.Hatchling.location(),
+]
+
+
+def split_list(list: list[T], index: int) -> tuple[list[T], list[T]]:
+    return list[:index], list[index:]
+
 
 def create_hints(world: SamusReturnsWorld):
-    hints: dict[HintData, list[Location | Item]] = {}
     forbidden_locations = set(world.prefilled_locations)
+
+    def get_hintable_locations(locations: Iterable[Location]):
+        valid_locations = [
+            loc
+            for loc in locations
+            if loc.address is not None
+            and loc.item is not None
+            and loc.progress_type != LocationProgressType.EXCLUDED
+            and loc.name not in world.options.start_location_hints.value
+            and loc.item.name not in world.options.start_hints.value
+        ]
+        world.random.shuffle(valid_locations)
+        return valid_locations
 
     def filter_forbidden(locations: Iterable[Location]):
         return filter(lambda location: location not in forbidden_locations, locations)
 
-    locations = [
-        loc
-        for loc in world.get_locations()
-        if loc.address is not None
-        and loc.item is not None
-        and loc.progress_type != LocationProgressType.EXCLUDED
-        and loc.name not in world.options.start_location_hints.value
-        and loc.item.name not in world.options.start_hints.value
-    ]
-    world.random.shuffle(locations)
-
-    item_locations = [
-        loc
-        for loc in world.multiworld.find_items_in_locations(major_items.keys(), world.player)
-        if loc.name not in world.options.start_location_hints.value
-        and cast(Item, loc.item).name not in world.options.start_hints.value
-    ]
-    world.random.shuffle(item_locations)
-
+    locations = get_hintable_locations(world.get_locations())
+    always_hint_locations = get_hintable_locations(world.get_location(loc) for loc in ALWAYS_HINTS)
+    item_locations = get_hintable_locations(world.multiworld.find_items_in_locations(major_items.keys(), world.player))
     if ItemName.MetroidDna in world.options.start_hints.value:
         dna_statue_count = 0
         dna_groups = []
     else:
-        dna_locations = [
-            loc
-            for loc in world.multiworld.find_item_locations(ItemName.MetroidDna, world.player)
-            if loc not in forbidden_locations
-            and loc.name not in world.options.start_location_hints.value
-            and cast(Item, loc.item).name not in world.options.start_hints.value
-        ]
+        dna_locations = get_hintable_locations(world.multiworld.find_item_locations(ItemName.MetroidDna, world.player))
         dna_locations = dna_locations[: world.options.dna_required]
-        world.random.shuffle(dna_locations)
         dna_statue_count = math.ceil(world.options.dna_required / DNA_HINTS_PER_STATUE)
         dna_groups: list[list[Location]] = [[] for _ in range(dna_statue_count)]
         # Round robin so the hints are evenly distributed
         for group, location in zip(itertools.cycle(dna_groups), dna_locations, strict=False):
             group.append(location)
 
+    hints: dict[HintData, list[Location | Item]] = {}
     _hints = list(hint_data)
     world.random.shuffle(_hints)
 
-    dna_hints = _hints[:dna_statue_count]
-    _hints = _hints[dna_statue_count:]
-    location_hints = _hints[: len(_hints) // 2]
-    item_hints = _hints[len(_hints) // 2 :]
+    dna_hints, _hints = split_list(_hints, dna_statue_count)
+    always_hints, _hints = split_list(_hints, len(ALWAYS_HINTS))
+    item_hints, location_hints = split_list(_hints, len(_hints) // 2)  # Bias to locations if uneven
 
     for hint, dna_group in zip(dna_hints, dna_groups, strict=False):
         hints[hint] = sorted((cast(Item, location.item) for location in dna_group), key=lambda location: location.name)
         forbidden_locations.update(dna_group)
+    for hint, location in zip(always_hints, filter_forbidden(always_hint_locations), strict=False):
+        hints[hint] = [location]
+        forbidden_locations.add(location)
     for hint, location in zip(location_hints, filter_forbidden(locations), strict=False):
         hints[hint] = [location]
         forbidden_locations.add(location)
